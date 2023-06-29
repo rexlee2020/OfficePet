837e18524e4b2382912d626de31dab7fccace91f
jongmin <jongmin@jongmins-MacBook-Air.local>
2023-06-29 18:19:37 +0900

    asdf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Routes } from "react-router-dom";
 import Intro from "./Intro";
 import Question from "./Question";
 import Result from "./Result";
